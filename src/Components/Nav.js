b1164eec77b965e87587c14662cb1e96c3a0a4a7
Dhanushk19 <95034453+Dhanushk19@users.noreply.github.com>
2022-11-08 21:34:15 +0530

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -24,7 +24,7 @@ function Nav()
             user == null ?
             (
           
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-0">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   
             <ul className="navbar-nav">
                <li className="navbar" style={{marginLeft:"50px"}}>
